fa340126771e3802f77c62c47b3c4bbbe7c41ac1
Plague Fox <plugfox@gmail.com>
2023-07-09 22:41:17 +0400

    Update StateConsumer

--- example/chat_app/lib/src/common/controller/state_consumer.dart
+++ example/chat_app/lib/src/common/controller/state_consumer.dart
@@ -1,7 +1,7 @@
 import 'package:chatapp/src/common/controller/state_controller.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
-import 'package:flutter/src/foundation/diagnostics.dart';
 
 /// Fire when the state changes.
 typedef StateConsumerListener<S> = void Function(BuildContext context, S previous, S current);
